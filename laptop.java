3c92e650da539f787b06e676c122734a7e266730
Dibyajyotinayak <35395206+Dibyajyotinayak@users.noreply.github.com>
2025-09-16 20:29:25 +0530

    Create laptop.java
    new program

---
+++ laptop.java
@@ -0,0 +1,27 @@
+class Laptop
+{
+	static String number="15-fd0552TU";
+	static String pnumber="BB7B8PA#ACJ";
+	static String color="Natural Silver";
+	static String type="thin and light laptop";
+	static String battery="3-cell,41 wh li-ion polymer";
+	static String processor="intel";
+	static String ssd="yes";
+	static String RAM="16 GB";
+	static String cache="12 MB";
+	static double price=10000.00;
+	public static void main(String[] args) 
+	{
+		System.out.println("***** Laptop Details*****");
+		System.out.println("laptop number"+number);
+		System.out.println("laptop pnumber"+ pnumber);
+		System.out.println("laptop  color"+ color);
+		System.out.println("laptop  type" +type);
+		System.out.println("laptop  battery" +battery);
+		System.out.println("laptop  processor" +processor);
+		System.out.println("laptop  ssd" +ssd);
+		System.out.println("laptop RAM" +RAM);
+		System.out.println("laptop cache" +cache);
+		System.out.println("laptop price" +price);
+	}
+}
